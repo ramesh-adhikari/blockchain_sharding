629924c0be1a66c413272478232814d142884fbf
Ramesh Adhikari <adramesh@Rameshs-MacBook-Pro.local>
2022-12-21 22:42:58 -0500

    cleanup the repo

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 from generator_scripts.bootstrap import Bootstrap
 from models.transaction import Transaction
 
-from transaction_utility import TransactionUtility
-
-
 if __name__ == '__main__':
 
     print("Hello World")
